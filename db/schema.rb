c9c02c196381b8b5c90b163a1cc95f25415fab96
Aimee Maher <amaher@itriagehealth.com>
2015-03-24 21:14:45 -0600

    removes contents table

--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 20150325031050) do
     t.datetime "updated_at"
   end
 
-  create_table "contents", force: true do |t|
-    t.string   "type"
-    t.string   "stage"
-    t.string   "value"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "diary_entries", force: true do |t|
     t.string   "uuid"
     t.string   "value"
